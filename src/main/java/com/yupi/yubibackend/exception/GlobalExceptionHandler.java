f2f9f54f2ea169795f71e6c3f818bcd8c55a39cb
xiaoshentongxue <xiaoshen@163.com>
2025-08-07 15:55:01 +0800

    1.项目初始化 新增: 全局异常处理 通过请求包装类 全局跨域配置

---
+++ src/main/java/com/yupi/yubibackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.yupi.yubibackend.exception;
+
+import com.yupi.yubibackend.common.BaseResponse;
+import com.yupi.yubibackend.common.ResultUtils;
+import com.yupi.yubibackend.exception.BusinessException;
+import com.yupi.yubibackend.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Hidden
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
